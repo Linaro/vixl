3b2c3fd578c8fcb68cb46e955e1b5216642c723f
Martyn Capewell <martyn.capewell@arm.com>
2022-04-14 15:02:27 +0100

    Remove per-instruction functions and data structures
    Improve static code/data size by:
    
     1) Removing the per-instruction visitor functions. The generic Visit()
        function can be overridden instead to intercept individual instructions, as
        shown in the existing custom disassembler example.
     2) Remove the array of instruction forms. Instead infer the forms from the
        decoder tree defined by kDecodeMapping.
    
    As it's now unused, remove the INSTRUCTION_VISITOR_LIST define too.

--- src/aarch64/disasm-aarch64.h
+++ src/aarch64/disasm-aarch64.h
@@ -126,7 +126,7 @@ class Disassembler : public DecoderVisitor {
   uint32_t form_hash_;
 
   void SetMnemonicFromForm(const std::string& form) {
-    if (form != "Unallocated") {
+    if (form != "unallocated") {
       VIXL_ASSERT(form.find_first_of('_') != std::string::npos);
       mnemonic_ = form.substr(0, form.find_first_of('_'));
     }
